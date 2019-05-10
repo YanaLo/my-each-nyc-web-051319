15139d940aad3fc881a221a79b280cff0a4ca50b
yana.loginova <yana.loginova@cix.csi.cuny.edu>
2019-05-10 04:58:59 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -6,9 +6,16 @@ def my_each(array) # put argument(s) here
   
   i+=1
  end 
+<<<<<<< HEAD
  array
 end
 
+=======
+  my_each(array) do |i|
+    i
+end
+end
+>>>>>>> 14b66accf2f2c8fab3bef78276875a5c0388c48d
 
 # my_each(array) do|i|
 # i  
